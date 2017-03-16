2a7e401965fd1fbe9fa1def9ff8f40b18c763cd5
Guru Prasad Srinivasa <gurupras@buffalo.edu>
2017-03-15 20:06:08 -0400

    Added device flag to specify PulseAudio device to capture and transmit

--- transmitter/main.go
+++ transmitter/main.go
@@ -14,20 +14,22 @@ import (
 )
 
 var (
-	addr  *string
-	proto *string
+	addr   *string
+	proto  *string
+	device *string
 )
 
 func setupParser() {
 	addr = kingpin.Arg("receiver-address", "Address of receiver").Required().String()
 	proto = kingpin.Flag("protocol", "tcp/udp").Short('P').Default("udp").String()
+	device = kingpin.Flag("alsa-device", "ALSA device from which to capture and transmit").Short('d').Default("alsa_output.pci-0000_00_05.0.analog-stereo.monitor").String()
 }
 func main() {
 	setupParser()
 	kingpin.Parse()
 	var err error
 
-	audioTransmitter := audiotransport.NewAudioTransmitter("transmitter", "alsa_output.pci-0000_00_05.0.analog-stereo.monitor", 48000, 2)
+	audioTransmitter := audiotransport.NewAudioTransmitter("transmitter", *device, 48000, 2)
 	if err = audioTransmitter.Connect(*proto, *addr); err != nil {
 		fmt.Fprintln(os.Stderr, fmt.Sprintf("Failed to connet to server: %v", err))
 		return
