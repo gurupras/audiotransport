9fa94c3c8847ac780dc933188d3d222c26fda04d
Guru Prasad Srinivasa <gurupras@buffalo.edu>
2017-03-24 08:35:15 -0400

    Fully removed alsa-bindings and alsa

--- alsa/doc.go
+++
@@ -1,7 +0,0 @@
-// WARNING: This file has automatically been generated on Tue, 14 Mar 2017 01:22:35 EDT.
-// By https://git.io/c-for-go. DO NOT EDIT.
-
-/*
-Go bindings for libasound
-*/
-package alsa
